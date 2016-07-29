d91770e275749f08709207bbd4d4a50cd88e1fc9
Tyler Arbon <tylercd100@gmail.com>
2016-07-28 21:25:16 -0600

    added File back

---
+++ src/Models/File.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace Zenapply\Upload\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+	protected $table;
+	protected $guarded = ['id'];
+
+	public function __construct(array $attributes = [])
+    {
+    	if(empty($this->table)){
+	        $this->table = config('upload.table');
+    	}
+        parent::__construct($attributes);
+    }
+
+// [
+// 	'mime'
+// 	'path'
+// 	'disk'
+// 	'filename'
+// 	'extension'
+// 	'fingerprint'
+// ]
+}
